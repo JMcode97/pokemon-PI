9054cf0c342bb3e9507ab8d3fffd0e274fb7f5c5
JMCode97 <doomed2506@gmail.com>
2023-05-07 17:31:51 -0400

    Details styles

--- client/src/views/Details/Details.jsx
+++ client/src/views/Details/Details.jsx
@@ -1,6 +1,7 @@
 import axios from "axios"
 import { useEffect, useState } from "react"
 import { useParams } from "react-router-dom"
+import styles from './styles.module.css'
 
 const Details = () => {
     const { pokemon } = useParams()
@@ -25,7 +26,64 @@ const Details = () => {
     
     return(
         <>
-            
+            <div
+            className={styles.container} >
+                <div
+                className={styles.detailsContainer} >
+                    <div
+                    className={styles.title} >
+                        <h1>{pokemonData?.name}</h1>
+                        <span>#{pokemonData?.id}</span>
+                    </div>
+                    <div
+                    className={styles.info} >
+                        <div>
+                            <img 
+                            id={styles.pokemonImage}
+                            src={pokemonData?.image} 
+                            alt={pokemonData?.name} />
+                        </div>
+                        <div
+                        className={styles.stats} >
+                            <div
+                            className={styles.infoStats} >
+                                <div
+                                className={styles.statsName} >
+                                    <span>hp</span>
+                                    <span>attack</span>
+                                    <span>defense</span>
+                                    <span>speed</span>
+                                    <span>height</span>
+                                    <span>weight</span>
+                                </div>
+                                <div
+                                className={styles.statsValue} >
+                                    <span>{pokemonData?.hp}</span>
+                                    <span>{pokemonData?.attack}</span>
+                                    <span>{pokemonData?.defense}</span>
+                                    <span>{pokemonData?.speed}</span>
+                                    <span>{pokemonData?.height}</span>
+                                    <span>{pokemonData?.weight}</span>              
+                                </div>
+                            </div> 
+                            <div
+                            className={styles.statsTypes} >
+                                {
+                                    pokemonData?.types.map((type, index) => {
+                                        return (
+                                            <span
+                                            key={index}
+                                            style={{ backgroundColor: `var(--${type})`}} >
+                                            {type}
+                                            </span>
+                                        )
+                                    })
+                                }
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </>
     )
 }
