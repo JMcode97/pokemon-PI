38c394467f690da48184fb4f3711d956825b76ed
JMCode97 <doomed2506@gmail.com>
2023-05-11 09:37:52 -0400

    Add comments in backend methods

--- api/src/controllers/pokemonsController.js
+++ api/src/controllers/pokemonsController.js
@@ -1,19 +1,19 @@
 const axios = require('axios')
 const { Pokemon, Type } = require('../db')
 
+// Funcion para crear un objeto de pokemon
 const createPokemonObj = (res) => {
-    // crear un objeto por cada pokemon
     let { id, name, weight, height } = res.data
     let pokemonObj = {}
-    // array de tipos de pokemon
+    // Crear un array con los tipos del pokemon
     let pokemonTypes = res.data.types.map(data => data.type.name)
-    // insertar cada stat con su valor en el objeto de pokemon
+    // Insertar cada stat con su valor en el objeto de pokemon
     res.data.stats.map(el => {
         if(el.stat.name === 'hp' || el.stat.name === 'attack' || el.stat.name === 'defense' || el.stat.name === 'speed') {
             return pokemonObj[el.stat.name] = el.base_stat
         }
     })
-    // armar el objeto de pokemon con toda la informacion necesaria
+    // Armar el objeto de pokemon con toda la informacion necesaria
     return pokemonObj = {
         ...pokemonObj,
         id,
@@ -24,11 +24,13 @@ const createPokemonObj = (res) => {
         types: pokemonTypes,
     }
 }
-// armar el objeto de pokemon con informacion obtenida con Sequelize
+// Funcion para crear un objeto de pokemon con informacion obtenida de Sequelize
 const createPokemonObjDB = (res) => {
     let { hp, attack, defense, speed, id, name, image, weight, height, Types } = res[0].dataValues
     let pokemonObj = {}
+    // Crear un array con los tipos del pokemon
     let pokemonTypes = Types.map(data => data.dataValues.name)
+    // Armar el objeto con toda la informacion necesaria
     return pokemonObj = {
         hp,
         attack,
@@ -45,8 +47,10 @@ const createPokemonObjDB = (res) => {
 
 module.exports = {
     getAllPokemons: async (name) => {
+        // Verificar si se recibe una query para buscar pokemon
         const allPokemons = []
         if(name) {
+            // Busqueda de pokemon solicitado por query en la DB
             let pokemon
             await Pokemon.findOne({
                 where: {name},
@@ -58,13 +62,14 @@ module.exports = {
             .then(res => {
                 if(res) pokemon = createPokemonObjDB([res])
             })
+            // Si no existe en la DB se busca en la API
             if(!pokemon) {
                 await axios.get(`https://pokeapi.co/api/v2/pokemon/${name}`)
                 .then(res => pokemon = createPokemonObj(res))
             }
             return pokemon
         }
-        // obtener pokemons de la base de datos y guardarlos en el array
+        // Obtener pokemons de la base de datos y guardarlos en el array
         await Pokemon.findAll({
             include: {
                     model: Type,
@@ -76,14 +81,13 @@ module.exports = {
                 responses.map(res => allPokemons.push(createPokemonObjDB([res])))
             )
         })
-        // obtener url dentro de cada pokemon
-        
+        // Obtener url dentro de cada pokemon
         let URL = 'https://pokeapi.co/api/v2/pokemon?offset=0&limit=60'
         let getPokemons = await axios.get(URL)
         let pokemonsUrl = getPokemons.data.results.map(data => data.url)
-        // array de promesas
+        // Array de promesas
         let requests = pokemonsUrl.map(url => axios.get(url))
-        // llamado al array de promesas para obtener informacion de cada pokemon
+        // Llamado al array de promesas para obtener informacion de cada pokemon
         await axios.all(requests)
         .then(responses => {
             responses.forEach(res => {
@@ -92,17 +96,19 @@ module.exports = {
             })
         })
 
+        // Funcion para obtener todos los pokemons por lotes
+
         // let current = 'https://pokeapi.co/api/v2/pokemon?offset=0&limit=100'
         // while(current) {
         //     let getPokemons = await axios.get(current)
         //     let pokemonsUrl = getPokemons.data.results.map(data => data.url)
-        //     // array de promesas
+        //     // Array de promesas
         //     let requests = pokemonsUrl.map(url => axios.get(url))
-        //     // llamado al array de promesas para obtener informacion de cada pokemon
+        //     // Llamado al array de promesas para obtener informacion de cada pokemon
         //     await axios.all(requests)
         //     .then(responses => {
         //         responses.forEach(res => {
-        //             // crear objeto pokemon y guardarlo en array
+        //             // Crear objeto pokemon y guardarlo en array
         //             allPokemons.push(createPokemonObj(res))
         //         })
         //     })
@@ -113,8 +119,11 @@ module.exports = {
         return allPokemons
     },
     getById: async (idPokemon) => {
+        // regEx pra verificar si el ID es de tipo UUIDv4
+        let regexUUID = /^[0-9a-fA-F]{8}\b-[0-9a-fA-F]{4}\b-[0-9a-fA-F]{4}\b-[0-9a-fA-F]{4}\b-[0-9a-fA-F]{12}$/gi
+        // Si es UUIDv4 se hace la busqueda en la DB
         let getPokemon
-        if(idPokemon.length > 4) {
+        if(regexUUID.test(idPokemon)) {
             await Pokemon.findOne({
                 where: {id: idPokemon},
                 include: {
@@ -124,6 +133,7 @@ module.exports = {
             })
             .then(res => getPokemon = createPokemonObjDB([res]))
         }
+        // Si no es UUIDv4 se hace la busqueda en la API
         if(!getPokemon) {
             getPokemon = await axios.get(`https://pokeapi.co/api/v2/pokemon/${idPokemon}`)
             .then(res => createPokemonObj(res)) 
@@ -132,11 +142,12 @@ module.exports = {
     },
     createPokemon: async (name, image, hp, attack, defense, speed, height, weight, types) => {
         name = name.toLowerCase()
+        // Busqueda de los tipos obtenidos por form para obtener su id
         let getTypes = await Type.findAll({
             where: {name: types},
         })
         getTypes = getTypes.map(el => el.id)
-
+        // Creacion del pokemon en la DB
         let [pokemon, created] = await Pokemon.findOrCreate({
             where: { name },
             defaults: {
@@ -150,10 +161,13 @@ module.exports = {
                 weight
             },
         })
+        // Si ya el pokemon existe en la DB, retorna un mensaje
         if(!created) {
           return 'Pokemon already exists...'  
         }
+        // Se crea la relacion del pokemon creado y los tipos recibidos por form
         await pokemon.addTypes(getTypes)
+        // Se busca el pokemon recien creado para crear objeto y retornarlo
         await Pokemon.findOne({
             where: {name},
             include: {
--- api/src/controllers/typesController.js
+++ api/src/controllers/typesController.js
@@ -3,12 +3,14 @@ const { Type } = require('../db')
 
 module.exports = {
     getAllTypes: async () => {
+        // Obtener todos los tipos de pokemons de la API
         const allTypes = []
         let URL = 'https://pokeapi.co/api/v2/type'
         await axios.get(URL)
         .then(res => {
             res.data.results.map(el => allTypes.push(el.name))
         })
+        // Guardar cada tipo de pokemon en la DB
         await Promise.all(allTypes.map(type => {
             Type.findOrCreate({
                 where: {name: type},
@@ -17,6 +19,7 @@ module.exports = {
                 }
             })
         }))
+        // Obtener todos los tipos de pokemons de la DB y retornarlos
         const typesDB = await Type.findAll()
 
         return typesDB
